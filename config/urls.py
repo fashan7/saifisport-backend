a5a1448716e0e6129e881eb5dd546c3add67d502
fashan7 <fashanzak4@gmail.com>
2026-04-02 23:00:27 +0200

    fix: removed health check

--- config/urls.py
+++ config/urls.py
@@ -2,13 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('config.api_router')),
-    path('health/', lambda r: HttpResponse('ok')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
